8cdf5d8bfc58cbbc905d2e34232891cd1f200c9c
RodrigoAlvarezBerlanda <73083492+RodrigoAlvarezBerlanda@users.noreply.github.com>
2020-10-20 16:17:34 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,22 @@
+#This function assummes that array supplied is inverted. If not the function will
+#return a NULL value.
+
 makeCacheMatrix <- function(x= matrix()){
   inv <- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
+#Here we get the values of the matrix, the values of the inverse using a double error
+#operator (<<). After we get the values of the inverse a list is created including
+#these values.
   }
   get <- function() {x}
   setInverse <- function(inverse) {inv <<- inverse}
   getInverse <- function() {inv}
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
+#This function computes the inverse values of the matrix. First this check if
+#the matrix has already been inverted, if not it will get the inverse.
 
 cacheSolve <- function(x, ...){
   inv <- x$getInverse()
